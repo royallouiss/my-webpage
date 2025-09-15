40aa24f30370b8f16ee9fa03541828a97cefa03f
Royal Louis <royallouis1003@gmail.com>
2025-09-15 12:18:27 +0530

    changes

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world!\n");
+const http = require('http');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
